3336a8008c5a24138669b41c96269b6d9a4af497
Diego <58009401+marzo245@users.noreply.github.com>
2025-02-02 13:49:46 -0500

    fix error loop

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -4,65 +4,71 @@ import java.util.Scanner;
 
 public class Control extends Thread {
 
-    private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
-    private final static int TMILISECONDS = 5000;
+  private static final int NTHREADS = 3;
+  private static final int MAXVALUE = 30000000;
+  private static final int TMILISECONDS = 50;
 
-    private final int NDATA = MAXVALUE / NTHREADS;
-    private final PrimeFinderThread[] pft;
-    private final Object lock = new Object(); // Monitor de sincronización
+  private final int NDATA = MAXVALUE / NTHREADS;
+  private final PrimeFinderThread[] pft;
+  private final Object lock = new Object(); // Monitor de sincronización
 
-    private Control() {
-        super();
-        this.pft = new PrimeFinderThread[NTHREADS];
+  private Control() {
+    super();
+    this.pft = new PrimeFinderThread[NTHREADS];
 
-        int i;
-        for (i = 0; i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, lock);
-            pft[i] = elem;
-        }
-        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, lock);
+    int i;
+    for (i = 0; i < NTHREADS - 1; i++) {
+      PrimeFinderThread elem = new PrimeFinderThread(
+        i * NDATA,
+        (i + 1) * NDATA,
+        lock
+      );
+      pft[i] = elem;
+    }
+    pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, lock);
+  }
+
+  public static Control newControl() {
+    return new Control();
+  }
+
+  @Override
+  public void run() {
+    for (PrimeFinderThread thread : pft) {
+      thread.start();
     }
 
-    public static Control newControl() {
-        return new Control();
-    }
+    Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public void run() {
+    while (true) {
+      try {
+        Thread.sleep(TMILISECONDS);
+
+        // Pausar todos los hilos
         for (PrimeFinderThread thread : pft) {
-            thread.start();
+          thread.pauseThread();
         }
 
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            try {
-                Thread.sleep(TMILISECONDS);
-
-                // Pausar todos los hilos
-                for (PrimeFinderThread thread : pft) {
-                    thread.pauseThread();
-                }
-
-                // Mostrar cantidad de primos encontrados
-                int totalPrimes = 0;
-                for (PrimeFinderThread thread : pft) {
-                    totalPrimes += thread.getPrimes().size();
-                }
-                System.out.println("\nPrimos encontrados hasta ahora: " + totalPrimes);
-                System.out.println("Presione ENTER para continuar...");
-
-                scanner.nextLine(); // Espera la entrada del usuario
-
-                // Reanudar todos los hilos
-                for (PrimeFinderThread thread : pft) {
-                    thread.resumeThread();
-                }
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        // Mostrar cantidad de primos encontrados
+        int totalPrimes = 0;
+        for (PrimeFinderThread thread : pft) {
+          totalPrimes += thread.getPrimes().size();
         }
+        System.out.println("\nPrimos encontrados hasta ahora: " + totalPrimes);
+        System.out.println("Presione ENTER para continuar...");
+        if (totalPrimes == 1857859) {
+          System.out.println("Se encontraron todos los primos");
+          break;
+        }
+        scanner.nextLine(); // Espera la entrada del usuario
+
+        // Reanudar todos los hilos
+        for (PrimeFinderThread thread : pft) {
+          thread.resumeThread();
+        }
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
+  }
 }
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -5,66 +5,66 @@ import java.util.List;
 
 public class PrimeFinderThread extends Thread {
 
-    int a, b;
-    private List<Integer> primes;
-    private final Object lock;
+  int a, b;
+  private List<Integer> primes;
+  private final Object lock;
 
-    private boolean paused = false;
+  private boolean paused = false;
 
-    public PrimeFinderThread(int a, int b, Object lock) {
-        super();
-        this.primes = new LinkedList<>();
-        this.a = a;
-        this.b = b;
-        this.lock = lock;
-    }
+  public PrimeFinderThread(int a, int b, Object lock) {
+    super();
+    this.primes = new LinkedList<>();
+    this.a = a;
+    this.b = b;
+    this.lock = lock;
+  }
 
-    @Override
-    public void run() {
-        for (int i = a; i < b; i++) {
-            synchronized (lock) {
-                while (paused) {
-                    try {
-                        lock.wait(); // Espera hasta que Control lo reactive
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            if (isPrime(i)) {
-                primes.add(i);
-                System.out.println(i);
-            }
+  @Override
+  public void run() {
+    for (int i = a; i < b; i++) {
+      synchronized (lock) {
+        while (paused) {
+          try {
+            lock.wait(); // Espera hasta que Control lo reactive
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
         }
+      }
+      if (isPrime(i)) {
+        primes.add(i);
+        // System.out.println(i);
+      }
     }
+  }
 
-    boolean isPrime(int n) {
-        boolean ans;
-        if (n > 2) {
-            ans = n % 2 != 0;
-            for (int i = 3; ans && i * i <= n; i += 2) {
-                ans = n % i != 0;
-            }
-        } else {
-            ans = n == 2;
-        }
-        return ans;
+  boolean isPrime(int n) {
+    boolean ans;
+    if (n > 2) {
+      ans = n % 2 != 0;
+      for (int i = 3; ans && i * i <= n; i += 2) {
+        ans = n % i != 0;
+      }
+    } else {
+      ans = n == 2;
     }
+    return ans;
+  }
 
-    public List<Integer> getPrimes() {
-        return primes;
-    }
+  public List<Integer> getPrimes() {
+    return primes;
+  }
 
-    public void pauseThread() {
-        synchronized (lock) {
-            paused = true;
-        }
+  public void pauseThread() {
+    synchronized (lock) {
+      paused = true;
     }
+  }
 
-    public void resumeThread() {
-        synchronized (lock) {
-            paused = false;
-            lock.notifyAll(); // Reactivar todos los hilos en espera
-        }
+  public void resumeThread() {
+    synchronized (lock) {
+      paused = false;
+      lock.notifyAll(); // Reactivar todos los hilos en espera
     }
+  }
 }
