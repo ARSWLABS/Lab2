dc8f2729c3ecbbc311fd8c43e80b7b0c6b0e0ce8
Juan <112003348+juanescan@users.noreply.github.com>
2025-02-02 00:03:04 -0500

    funcionalidad de wait y norify hecha

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
+import java.util.Scanner;
+
 public class Control extends Thread {
-    
-    private final static int NTHREADS = 1;
+
+    private final static int NTHREADS = 3;
     private final static int MAXVALUE = 30000000;
     private final static int TMILISECONDS = 5000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
+    private final PrimeFinderThread[] pft;
+    private final Object lock = new Object(); // Monitor de sincronización
 
-    private PrimeFinderThread pft[];
-    
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+        this.pft = new PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+        for (i = 0; i < NTHREADS - 1; i++) {
+            PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, lock);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, lock);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
-            pft[i].start();
+        for (PrimeFinderThread thread : pft) {
+            thread.start();
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                Thread.sleep(TMILISECONDS);
+
+                // Pausar todos los hilos
+                for (PrimeFinderThread thread : pft) {
+                    thread.pauseThread();
+                }
+
+                // Mostrar cantidad de primos encontrados
+                int totalPrimes = 0;
+                for (PrimeFinderThread thread : pft) {
+                    totalPrimes += thread.getPrimes().size();
+                }
+                System.out.println("\nPrimos encontrados hasta ahora: " + totalPrimes);
+                System.out.println("Presione ENTER para continuar...");
+
+                scanner.nextLine(); // Espera la entrada del usuario
+
+                // Reanudar todos los hilos
+                for (PrimeFinderThread thread : pft) {
+                    thread.resumeThread();
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-    
 }
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,45 +3,68 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-                this.primes = new LinkedList<>();
-		this.a = a;
-		this.b = b;
-	}
+    int a, b;
+    private List<Integer> primes;
+    private final Object lock;
 
-        @Override
-	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
-                    primes.add(i);
+    private boolean paused = false;
+
+    public PrimeFinderThread(int a, int b, Object lock) {
+        super();
+        this.primes = new LinkedList<>();
+        this.a = a;
+        this.b = b;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        for (int i = a; i < b; i++) {
+            synchronized (lock) {
+                while (paused) {
+                    try {
+                        lock.wait(); // Espera hasta que Control lo reactive
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-            System.out.println(primes.size());
-	}
-	
-	boolean isPrime(int n) {
-	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
+            if (isPrime(i)) {
+                primes.add(i);
+                System.out.println(i);
             }
-	    return ans;
-	}
+        }
+    }
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
+    boolean isPrime(int n) {
+        boolean ans;
+        if (n > 2) {
+            ans = n % 2 != 0;
+            for (int i = 3; ans && i * i <= n; i += 2) {
+                ans = n % i != 0;
+            }
+        } else {
+            ans = n == 2;
+        }
+        return ans;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public void pauseThread() {
+        synchronized (lock) {
+            paused = true;
+        }
+    }
+
+    public void resumeThread() {
+        synchronized (lock) {
+            paused = false;
+            lock.notifyAll(); // Reactivar todos los hilos en espera
+        }
+    }
 }
